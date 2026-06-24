f9e47624c2d27b291c7c7c9240eaf4f8b29359a3
tuttuwucha <tuttuwucha@gmail.com>
2026-06-25 08:31:31 +1000

    modified:   Number Guessing Game.cpp 	new file:   a.out

--- Number Guessing Game.cpp
+++ Number Guessing Game.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
     srand(time(0));
-    int num = (rand() % 10) + 1;
+    int num = (rand() % 100) + 1;
     int guess;
+    std::string sguess;
     int tries = 0;
 
-
-    std::cout << "Guess a number between 1 and 10\n";
+    std::cout << '\n';
+    std::cout << "Guess a number between 1 and 100\n";
+    std::cout << '\n';
 
     do{
 
         std::cout << "Guess: ";
-        std::cin >> guess;
-        std::cout << '\n';
+        std::getline(std::cin >> std::ws, sguess);
+        try{
+            guess = stoi(sguess);
+        }
+        catch(const std::invalid_argument& e){
+            std::cout << "Invalid input!\n\n";
+            continue;
+        }
+
         tries++;
         if(guess < num){
-            std::cout << "The number is greater than your guess\n";
+            std::cout << "The number is greater than your guess\n\n";
         }
         else if(guess > num){
-            std::cout << "The number is smaller than your guess\n";
+            std::cout << "The number is smaller than your guess\n\n";
         }
 
     } while(guess != num);
 
 
     std::cout << "You won! It took you " << tries << " tries to guess the number.\n";
-
+    std::cout << '\n';
 
     return 0;
 }
